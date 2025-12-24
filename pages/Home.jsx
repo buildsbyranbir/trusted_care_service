efe8b4f0335da03b7f3cb22969197d30ac0a8b42
Ranbir <ranbir.webdev@gmail.com>
2025-12-24 21:28:39 +0600

    remove extra file and some changes

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -9,7 +9,6 @@ import {
 } from 'lucide-react';
 
 import { SERVICES, SERVICE_ICONS } from '../constants';
-import { getCareAdvice } from '../services/geminiService';
 
 const Home = () => {
   const [aiPrompt, setAiPrompt] = useState('');
